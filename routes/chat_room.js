c9c80bf78be4906f869f98fcd5443ba72b3f692f
sibitmathew <sibimathewtkv@gmail.com>
2016-01-04 13:26:42 +0530

    Change commit9

--- routes/chat_room.js
+++ routes/chat_room.js
@@ -12,8 +12,8 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");  
 //app.set("view options", {layout: false});  //This one does the trick for rendering static html
 //app.engine('html', require('ejs').renderFile); 
-//var http = require('http').Server(app);
-var http = require('http');
+var http = require('http').Server(app);
+//var http = require('http');
 
 var io = require('socket.io')(http);
 
@@ -42,14 +42,14 @@ router.get('/', function(req, res){
   
 });
 
-http.createServer(app).listen(app.get('port') ,app.get('ipaddr'), function () {
-    console.log("Express server listening at %s:%d ", app.get('ipaddr'),app.get('port'));
-    server();
-});
+// http.createServer(app).listen(app.get('port') ,app.get('ipaddr'), function () {
+//     console.log("Express server listening at %s:%d ", app.get('ipaddr'),app.get('port'));
+//     server();
+// });
 
-       // http.listen(3002, function(req,res){
-       //    console.log('listening on :5000');
-       //   });
+        http.listen(3002, function(req,res){
+           console.log('listening on :5000');
+          });
 
 
 
